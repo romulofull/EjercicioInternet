0857129d17231fdb6af47fb4712684a4f5ef9023
romulofull <romulo.castro@gmail.com>
2024-07-19 13:48:51 -0500

    Modificando el mensaje a Manzana, Platano, Uva

--- src/main/java/com/cursojava/curso/controllers/UsuariosController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuariosController.java
@@ -4,11 +4,13 @@ package com.cursojava.curso.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuariosController {
 @RequestMapping(value = "prueba")
-     public String prueba() {
-     return "Voy probando 2";
+     public List<String> prueba() {
+     return List.of("Manzana", "Platano", "Uvas");
 }
 
 }
