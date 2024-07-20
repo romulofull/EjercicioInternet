df2ad29c769ee66bc5f9ed2360da77a500db9859
romulofull <romulo.castro@gmail.com>
2024-07-19 20:04:54 -0500

    ejercicio Json

--- src/main/java/com/cursojava/curso/controllers/UsuariosController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuariosController.java
@@ -1,16 +1,25 @@
 package com.cursojava.curso.controllers;
 
 
+import com.cursojava.curso.models.Usuarios;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+
 
 @RestController
 public class UsuariosController {
+
 @RequestMapping(value = "prueba")
-     public List<String> prueba() {
-     return List.of("Manzana", "Platano", "Uvas");
+     public Usuarios getUsuarios() {
+     Usuarios usuario = new Usuarios();
+     usuario.setNombre("Romulo");
+     usuario.setApellido("Car");
+     usuario.setEmail("romulo.castro");
+     usuario.setTelefono("romulo");
+     usuario.setPassword("carlos");
+     return usuario;
 }
+
 
 }
